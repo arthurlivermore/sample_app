3f0c480c616d4556838581a3fd68226c79d77c12
Arthur Livermore <arthur@oregoncoastnews.tv>
2012-09-20 16:35:40 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1b6e9dbc86d6ad7210b9ccc5acc3767e7e4dfa16f19b11de591b35d439d3c99874d6e3e5b27eb8b60bead1bb7216ab7a08ec8d7faa43a1fb54de2fbdd18a74bf'
