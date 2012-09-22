7e04d99bf16f31c5a41eb9dc557fa15958188b64
Arthur Livermore <arthur@oregoncoastnews.tv>
2012-09-22 16:55:54 -0700

    Add root page to routes.db

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ get "static_pages/contact"
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
